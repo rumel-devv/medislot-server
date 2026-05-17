75fbec1c4a666f80028babed59236616af7f742d
rumel-devv <mdrumel.dev@gmail.com>
2026-05-18 00:36:50 +0600

    insert data at database

--- index.js
+++ index.js
@@ -1,7 +1,46 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
+app.use(cors())
+app.use(express.json());
+dotenv.config();
 const port = process.env.PORT || 5000 ;
+const uri =process.env.MONGODB_URI
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+     const db = client.db("medisolt");
+    const doctorsCollection = db.collection("doctors");
+
+
+
+
+    app.get("/doctors", async (req, res) => {
+      const result = await doctorsCollection.find().toArray();
+      res.json(result);
+    });
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('Server runnign fine')
